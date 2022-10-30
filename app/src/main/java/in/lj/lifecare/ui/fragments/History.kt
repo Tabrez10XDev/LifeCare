26351a59aa83aa124cc924896c918246cc0fc346
Tabrez <meet.tabrezt1@gmail.com>
2022-10-30 23:58:29 +0530

    Done Alpha0.1 TODO: _ Mood: Bitter

--- app/src/main/java/in/lj/lifecare/ui/fragments/History.kt
+++ app/src/main/java/in/lj/lifecare/ui/fragments/History.kt
@@ -1,15 +1,16 @@
 package `in`.lj.lifecare.ui.fragments
 
-import `in`.lj.lifecare.DateBindingModel_
-import `in`.lj.lifecare.DateHistoryBindingModel_
+import `in`.lj.lifecare.*
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import `in`.lj.lifecare.R
-import `in`.lj.lifecare.dateHistory
+import `in`.lj.lifecare.data.Booking
+import android.widget.Toast
 import com.airbnb.epoxy.CarouselModel_
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_history.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -17,6 +18,8 @@ import java.util.*
 
 class History : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+    private var fireStore : FirebaseFirestore = FirebaseFirestore.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,8 +30,13 @@ class History : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        getBookings()
         rvController()
     }
+
+
+
 
     private fun rvController(){
         rvHistory.withModels {
@@ -56,6 +64,43 @@ class History : Fragment() {
                 .models(dateHistoryModel)
                 .addTo(this);
         }
+    }
+
+    private fun rvController2(bookings : MutableList<Booking>){
+        rvHistory2.withModels {
+            bookings.forEachIndexed { index, booking ->
+                var status = "Completed"
+                if(booking.status)
+                    status = "Currently Ongoing"
+                historyCard {
+                    id(index)
+                    name(booking.doctorName)
+                    dept(booking.doctorDept)
+                    status(status)
+                    date(booking.ddmmyy)
+                }
+            }
+        }
+    }
+
+    private fun getBookings(){
+        val itemList : MutableList<Booking> = arrayListOf()
+        fireStore.collection("users")
+            .document(auth.uid!!)
+            .collection("appointments")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    itemList.add(
+                        document.toObject(Booking::class.java)
+                    )
+
+                }
+                rvController2(itemList)
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(requireContext(), exception.localizedMessage, Toast.LENGTH_SHORT).show()
+            }
     }
 
 
